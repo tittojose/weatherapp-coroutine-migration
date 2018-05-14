e5fe8de9a5a8d83bc2691068ac6a58bb2a415070
Titto Jose <mailtitto@gmail.com>
2018-05-15 00:34:10 +0530

    remove commented code

--- app/src/main/java/weatherapp/tittojose/me/weatherapp/ui/presenter/HomeScreenPresenter.java
+++ app/src/main/java/weatherapp/tittojose/me/weatherapp/ui/presenter/HomeScreenPresenter.java
@@ -1,6 +1,5 @@
 package weatherapp.tittojose.me.weatherapp.ui.presenter;
 
-import weatherapp.tittojose.me.weatherapp.model.WeatherAPI;
 import weatherapp.tittojose.me.weatherapp.model.pojo.WeatherModel;
 import weatherapp.tittojose.me.weatherapp.repository.WeatherRepositoryContract;
 import weatherapp.tittojose.me.weatherapp.ui.view.HomeScreen;
@@ -36,30 +35,6 @@ public class HomeScreenPresenter implements HomePresenterContract {
     public void loadWeatherData() {
         this.homeScreen.showLoading();
         this.weatherRepository.getWeatherData();
-
-//        this.homeScreen.showLoading();
-//
-//
-//        this.weatherAPI.getWeatherForecast().enqueue(new Callback<WeatherModel>() {
-//            @Override
-//            public void onResponse(Call<WeatherModel> call, Response<WeatherModel> response) {
-//                homeScreen.hideLoading();
-//                if (response.isSuccessful()) {
-//                    homeScreen.onWeatherDataLoadSuccess(response.body());
-//                } else {
-//                    homeScreen.onWeatherDataLoadFailed();
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<WeatherModel> call, Throwable t) {
-//                homeScreen.hideLoading();
-//                homeScreen.onWeatherDataLoadFailed();
-//            }
-//        });
-
-//        this.homeScreen.showLoading();
-//        getObservable().subscribeWith(getObserver());
     }
 
     @Override
